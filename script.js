d436dbff65961cb1daaccd54546172abae66e2f0
Marcus <96802250+Marcus8843@users.noreply.github.com>
2024-11-06 02:59:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const button = document.getElementById('codigos')
+
+button.addEventListener('click', () => {
+    const link = "https://github.com/Marcus8843/Marcus8843"
+    window.location.href = link;
+})
+})
